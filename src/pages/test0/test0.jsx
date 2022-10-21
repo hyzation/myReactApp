597da9c239ad5c4a9166d721297e4599a20f4aa4
何逸洲 <www.hyz978030720@sina.com>
2022-10-21 16:24:03 +0800

    s

--- src/pages/test0/test0.jsx
+++ src/pages/test0/test0.jsx
@@ -1,7 +1,74 @@
+import React, {
+    useRef,
+    Suspense,
+} from "react";
+import {
+    OrbitControls,
+    PerspectiveCamera,
+    Environment,
+    // useHelper,
+} from "@react-three/drei";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// import {
+// PointLightHelper,
+// } from "three";
+import {
+    // useFrame,
+    useLoader,
+} from "@react-three/fiber";
+// import { Perf } from "r3f-perf";
+// import {useControls, folder} from "leva";
+
 export default function Index() {
+    const pointLightPosition = [0, 10, 10];
+    const cameraPosition = [20, 10, 0];
+    // const cameraRotation = [Math.PI / 3, Math.PI / 2, Math.PI / 3];
+    const env = '/resource/env/view_1.hdr';
+
+    const gltf = useLoader(GLTFLoader, '/model/spaceship.glb');
+    const scale = 1
+
+    const cam = useRef();
+    // const pointLight1 = useRef();
+    // useHelper(pointLight1, PointLightHelper, 5, "red");
+
+    const controls = useRef();
+
     return (
         <>
-            000
+            <PerspectiveCamera makeDefault ref={cam}
+                position={cameraPosition}
+                fov={75} far={2000}
+            />
+
+            <pointLight
+                // ref={pointLight1}
+                position={pointLightPosition}
+                intensity={1}
+                distance={30}
+            />
+
+            <Suspense fallback={null}>
+                <primitive object={gltf.scene} scale={[scale, scale, scale]} />
+            </Suspense>
+
+            <OrbitControls
+                ref={controls}
+                autoRotate={true}
+                autoRotateSpeed={1}
+                camera={cam.current}
+                target={[0, 0, 0]}
+                // makeDefault
+                // minAzimuthAngle={-Math.PI}
+                // maxAzimuthAngle={Math.PI / 4}
+                minPolarAngle={0}
+                maxPolarAngle={Math.PI / 3}
+                enableZoom={true}
+                minDistance={10}
+                maxDistance={30}
+                enablePan={false}
+            // zoomSpeed={0.3}
+            />
         </>
     );
 }
--- src/routes/test0.jsx
+++ src/routes/test0.jsx
@@ -1,9 +1,19 @@
 import Test0 from "../pages/test0/test0";
+import React, { Suspense } from "react";
+import { Html } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 export default function Index() {
     return (
         <>
-            <Test0 />
+            <Canvas style={{ width: '100%', height: '100vh', }}>
+                <Suspense fallback={<Html style={{
+                    color: '#000000',
+                    width: '100px',
+                }} center>加载中...</Html>}>
+                    <Test0 />
+                </Suspense>
+            </Canvas>
         </>
     );
 }
