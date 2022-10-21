81a0c484c94dc1708a06f8f98da1cf2e54060b83
何逸洲 <www.hyz978030720@sina.com>
2022-10-21 16:25:59 +0800

    s

--- src/routes/test0.jsx
+++ src/routes/test0.jsx
@@ -1,7 +1,7 @@
-import Test0 from "../pages/test0/test0";
 import React, { Suspense } from "react";
 import { Html } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import Test0 from "../pages/test0/test0";
 
 export default function Index() {
     return (
