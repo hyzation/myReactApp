58b1c01b5d3a745a19fdbcfa0176d18059007d0e
何逸洲 <www.hyz978030720@sina.com>
2023-03-02 09:00:31 +0800

    s

--- src/routes/test0.jsx
+++ src/routes/test0.jsx
@@ -6,14 +6,7 @@ import Test0 from "../pages/test0/test0";
 export default function Index() {
     return (
         <>
-            <Canvas style={{ width: '100%', height: '100vh', }}>
-                <Suspense fallback={<Html style={{
-                    color: '#000000',
-                    width: '100px',
-                }} center>加载中...</Html>}>
-                    <Test0 />
-                </Suspense>
-            </Canvas>
+            <Test0 />
         </>
     );
 }
